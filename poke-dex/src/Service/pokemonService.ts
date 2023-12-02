08ee22d723f8972a7ddc9c8615b04027f153b343
ssub0227 <sb010227@gmail.com>
2023-12-02 15:59:21 +0900

    Style: 지정 API responce Type

--- poke-dex/src/Service/pokemonService.ts
+++ poke-dex/src/Service/pokemonService.ts
@@ -1,12 +1,20 @@
 import axios from 'axios'
 
 const remote = axios.create()
-const fetchPokemons = async () =>{
+
+export interface PokemonListResponceType {
+  count: number,
+  next: string
+  results:{
+    name: string,
+    url: string,
+  }[]
+}
+
+export const fetchPokemons = async () =>{
   const defalutUrl = `https://pokeapi.co/api/v2/pokemon`
   
   // post, get, put, delete
-  const response = await remote.get(defalutUrl)
-  return response
+  const response = await remote.get<PokemonListResponceType>(defalutUrl)
+  return response.data
 }
-
-export default fetchPokemons
