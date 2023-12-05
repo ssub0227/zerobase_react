4ca95a17b7fdbbac9ed92b40ac231685463b4654
ssub0227 <sb010227@gmail.com>
2023-12-06 01:15:20 +0900

    Feat: Select 상태관리

--- poke-dex/src/App.tsx
+++ poke-dex/src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter } from 'react-router-dom';
 import PageNavigator from './PageNavigator'
 import PageHeader from './Common/PageHeader'
+import { store } from './Store/index'
+import { Provider } from 'react-redux'
 
 function App() {
   return (
+    <Provider store={store}> 
     <BrowserRouter>
       <PageHeader />
       <PageNavigator/ >
     </BrowserRouter>
+    </Provider>
   );
 }
 
--- poke-dex/src/Common/PageHeader.tsx
+++ poke-dex/src/Common/PageHeader.tsx
@@ -1,14 +1,27 @@
 import styled from '@emotion/styled'
 import { Link } from 'react-router-dom'
+import { ChangeEvent } from 'react'
+import { POKEMON_IMAGE_TYPE } from '../Constants'
+import { RootState, useAppDispatch } from '../Store'
+import { useSelector } from 'react-redux'
+import { PokemonImageKeyType, changeImageType } from '../Store/imageTypeSlice'
 
 const PageHeader =() =>{
+  const type = useSelector((state: RootState) => state.imageType.type)
+  const dispatch = useAppDispatch()
+  
+  const handleChange =(e:ChangeEvent<HTMLSelectElement>) =>{
+    dispatch(changeImageType({
+      type:e.target.value as PokemonImageKeyType
+    }))
+  }
   return (
     <Header>
       <Title><Link to='/'>Pokémon</Link></Title>
-      <Select>
-        <option value={'Official'}>Official</option>
-        <option value={'a'}>a</option>
-        <option value={'b'}>b</option>
+      <Select value={type} onChange={handleChange}>
+        <option value={POKEMON_IMAGE_TYPE.OFFICIAL_ARTWORK}>Official</option>
+        <option value={POKEMON_IMAGE_TYPE.DREAM_WORLD}>DreamWorld</option>
+        <option value={POKEMON_IMAGE_TYPE.FRONT_DEFAULT}>FrontDefault</option>
       </Select>
     </Header>
   )
